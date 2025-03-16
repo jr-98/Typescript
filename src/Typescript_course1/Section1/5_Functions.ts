6970e2319e6be8938a3fc85777243e473f0a5436
jr98 <jrtillaguango@gmail.com>
2025-03-16 01:16:41 -0500

    [Config project] Configurtacion projecto ts

--- src/Typescript_course1/Section1/5_Functions.ts
+++ src/Typescript_course1/Section1/5_Functions.ts
@@ -1,6 +1,14 @@
 function addition(a: number, b: number): number {
     return a + b
 }
-const price: number = addition(4, 6)
+//Functions without params
+function void_function(): void {
+    console.log('This is a function without params')
+}
 
+// #region Call functions 
+const price: number = addition(4, 6)
 console.log(price)
+
+void_function()
+// #endregion
