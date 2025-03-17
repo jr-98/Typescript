443ec9491e0fce112119c35f6b98feebf38fed12
jr98 <jrtillaguango@gmail.com>
2025-03-16 20:04:06 -0500

    [Feature] Clases 1

---
+++ src/Typescript_course1/Section3_Clases/!_Introduction.ts
@@ -0,0 +1,4 @@
+// The class are objects layouts, methods, properties from this objects
+// Object: Car
+// Properties: color, brand, number, number of wheels, number of doors
+// Methods: Engine start, acceleration, braking 
